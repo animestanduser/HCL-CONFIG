a40e9a50ff9e2c0e4dda1596883dcbe694b6551c
animestanduser <moniasty2late@gmail.com>
2022-05-23 09:52:44 +0200

    Add client and server

---
+++ server/src/test/java/configserver/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package configserver.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
